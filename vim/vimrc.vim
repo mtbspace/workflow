89dd43088c9042deb8e95c522b86fb1d8694627a
mtbspace <james@mtb.space>
2019-02-02 14:23:25 +0000

    Add comments about how files are sourced

---
+++ vim/vimrc.vim
@@ -0,0 +1,4 @@
+" ##################################################
+" # This file is sourced from ~/.vimrc as follows: #
+" # so /home/user/workflow/vim/vimrc.vim           #
+" ##################################################
--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,3 +1,8 @@
+##################################################
+# This file is sources from ~/.zshrc as follows: #
+# source '/home/user/workflow/zsh/zshrc.sh'      #
+##################################################
+
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
