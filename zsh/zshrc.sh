b134474a2c6db0f76019759b6c20e20d6d12746c
mtbspace <james@mtbspace>
2019-02-02 22:01:40 +0000

    Add history and completion settings

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -3,6 +3,19 @@
 # source '/home/user/workflow/zsh/zshrc.sh'      #
 ##################################################
 
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=10000
+SAVEHIST=10000
+bindkey -v
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/home/james/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
